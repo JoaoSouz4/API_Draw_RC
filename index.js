036131a2d62d9ee6550bd5fec572cf13c4f850a5
João Souza <joaosouz8@gmail.com>
2023-02-18 21:48:42 -0300

    correcao cors

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const app = require('./app');
+const express = require('express');
+const routes = require('./routes');
+const cors = require('cors');
+const app = express();
 const Loaders = require('./Loaders/index');
 require('dotenv').config();
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 Loaders.start();
 
 app.listen(process.env.PORT, () => console.log("Servidor rodando na porta: "+process.env.PORT));
