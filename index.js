86dd1561e01c6c8711732bcf45d91235d502ee86
João Souza <joaosouz8@gmail.com>
2023-02-27 23:57:43 -0300

    edição em uma foto

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const path = require('path')
 const routes = require('./routes');
 const cors = require('cors');
 const app = express();
 const Loaders = require('./Loaders/index');
 require('dotenv').config();
+
+app.use('/files', express.static(path.resolve(__dirname, "uploads")))
 app.use(cors());
 app.use(express.json());
 app.use(routes);
--- routes.js
+++ routes.js
@@ -1,12 +1,12 @@
 const { Router } = require("express");
 const DrawController = require("./Controllers/DrawController");
+
 const routes =  Router();
 
 routes.get('/', (req,res)=>{
     res.status(200).json({message: "ok"});
 });
-
-routes.post('/register', DrawController.store);
+routes.post('/store', DrawController.store)
 routes.get('/show', DrawController.show);
 
 module.exports = routes
